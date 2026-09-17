4a24e569f8359886c157707184f9f94087535bb0
LuisaPaez27 <luissafernandapaezmartinez@gmail.com>
2026-09-17 18:20:05 -0500

    microservicio salones

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -2,22 +2,16 @@ from database.db_config import get_connection
 from repositories.postgres_docente_repository import PostgresDocenteRepository
 from repositories.postgres_reserva_repository import PostgresReservaRepository
 from repositories.postgres_rol_repository import PostgresRolRepository
-from repositories.postgres_salon_repository import PostgresSalonRepository
 from repositories.postgres_usuario_repository import PostgresUsuarioRepository
 from security.werkzeug_password_hasher import WerkzeugPasswordHasher
 from services.docente_service import DocenteService
 from services.reserva_service import ReservaService
 from services.rol_service import RolService
-from services.salon_service import SalonService
 from services.usuario_service import UsuarioService
 
 
 def get_usuario_service() -> UsuarioService:
     return UsuarioService(PostgresUsuarioRepository(get_connection), WerkzeugPasswordHasher())
-
-
-def get_salon_service() -> SalonService:
-    return SalonService(PostgresSalonRepository(get_connection))
 
 
 def get_reserva_service() -> ReservaService:
