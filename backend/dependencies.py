79825686ca970d33f8bd37d06ee55e3f88dcb770
LuisaPaez27 <luissafernandapaezmartinez@gmail.com>
2026-09-17 18:20:06 -0500

    microservicio reservas

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,14 +1,8 @@
 from database.db_config import get_connection
-from repositories.postgres_reserva_repository import PostgresReservaRepository
 from repositories.postgres_usuario_repository import PostgresUsuarioRepository
 from security.werkzeug_password_hasher import WerkzeugPasswordHasher
-from services.reserva_service import ReservaService
 from services.usuario_service import UsuarioService
 
 
 def get_usuario_service() -> UsuarioService:
     return UsuarioService(PostgresUsuarioRepository(get_connection), WerkzeugPasswordHasher())
-
-
-def get_reserva_service() -> ReservaService:
-    return ReservaService(PostgresReservaRepository(get_connection))
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,6 @@ from fastapi.staticfiles import StaticFiles
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from controllers.reserva_controller import router as reserva_router
 from controllers.usuario_controller import router as usuario_router
 from controllers.vista_controller import router as vista_router
 
@@ -24,7 +23,6 @@ def create_app() -> FastAPI:
     )
     application.include_router(vista_router)
     application.include_router(usuario_router)
-    application.include_router(reserva_router)
     application.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
     return application
 
